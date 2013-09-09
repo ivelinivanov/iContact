cf1f2b7cb0b421ac1f64698156ea45c5fc566eda
ivelinivanov <ivelin.ivanov@mentormate.com>
2013-09-09 13:26:35 +0300

    Initial Commit

---
+++ iContact/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iContact
+//
+//  Created by Ivelin Ivanov on 9/9/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
